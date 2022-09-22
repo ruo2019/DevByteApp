de5af4793802bf029040108d277dda24151ace7c
ruo2019 <ruo2019@outlook.com>
2022-09-21 21:39:21 -0700

    stuff

--- app/build/tmp/kapt3/stubs/debug/com/example/android/devbyteviewer/repository/VideosRepository.java
+++ app/build/tmp/kapt3/stubs/debug/com/example/android/devbyteviewer/repository/VideosRepository.java
@@ -5,10 +5,25 @@ import java.lang.System;
 /**
  * Repository for fetching devbyte videos from the network and storing them on disk
  */
-@kotlin.Metadata(mv = {1, 6, 0}, k = 1, d1 = {"\u0000\f\n\u0002\u0018\u0002\n\u0002\u0010\u0000\n\u0002\b\u0002\u0018\u00002\u00020\u0001B\u0005\u00a2\u0006\u0002\u0010\u0002\u00a8\u0006\u0003"}, d2 = {"Lcom/example/android/devbyteviewer/repository/VideosRepository;", "", "()V", "app_debug"})
+@kotlin.Metadata(mv = {1, 6, 0}, k = 1, d1 = {"\u0000*\n\u0002\u0018\u0002\n\u0002\u0010\u0000\n\u0000\n\u0002\u0018\u0002\n\u0002\b\u0002\n\u0002\u0018\u0002\n\u0002\u0010 \n\u0002\u0018\u0002\n\u0002\b\u0003\n\u0002\u0010\u0002\n\u0002\b\u0002\u0018\u00002\u00020\u0001B\r\u0012\u0006\u0010\u0002\u001a\u00020\u0003\u00a2\u0006\u0002\u0010\u0004J\u0011\u0010\u000b\u001a\u00020\fH\u0086@\u00f8\u0001\u0000\u00a2\u0006\u0002\u0010\rR\u000e\u0010\u0002\u001a\u00020\u0003X\u0082\u0004\u00a2\u0006\u0002\n\u0000R\u001d\u0010\u0005\u001a\u000e\u0012\n\u0012\b\u0012\u0004\u0012\u00020\b0\u00070\u0006\u00a2\u0006\b\n\u0000\u001a\u0004\b\t\u0010\n\u0082\u0002\u0004\n\u0002\b\u0019\u00a8\u0006\u000e"}, d2 = {"Lcom/example/android/devbyteviewer/repository/VideosRepository;", "", "database", "Lcom/example/android/devbyteviewer/database/VideosDatabase;", "(Lcom/example/android/devbyteviewer/database/VideosDatabase;)V", "videos", "Landroidx/lifecycle/LiveData;", "", "Lcom/example/android/devbyteviewer/domain/DevByteVideo;", "getVideos", "()Landroidx/lifecycle/LiveData;", "refreshVideos", "", "(Lkotlin/coroutines/Continuation;)Ljava/lang/Object;", "app_debug"})
 public final class VideosRepository {
+    private final com.example.android.devbyteviewer.database.VideosDatabase database = null;
+    @org.jetbrains.annotations.NotNull()
+    private final androidx.lifecycle.LiveData<java.util.List<com.example.android.devbyteviewer.domain.DevByteVideo>> videos = null;
     
-    public VideosRepository() {
+    public VideosRepository(@org.jetbrains.annotations.NotNull()
+    com.example.android.devbyteviewer.database.VideosDatabase database) {
         super();
+    }
+    
+    @org.jetbrains.annotations.NotNull()
+    public final androidx.lifecycle.LiveData<java.util.List<com.example.android.devbyteviewer.domain.DevByteVideo>> getVideos() {
+        return null;
+    }
+    
+    @org.jetbrains.annotations.Nullable()
+    public final java.lang.Object refreshVideos(@org.jetbrains.annotations.NotNull()
+    kotlin.coroutines.Continuation<? super kotlin.Unit> continuation) {
+        return null;
     }
 }
